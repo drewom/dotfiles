82e2a1943a8cec598236d2982a807c51fc717874
Drew O'Malley <8580273+drewom@users.noreply.github.com>
2019-02-05 13:09:16 +0000

    Qutebrowser: Change mpv shortcuts to 'x', add ddg.

--- qutebrowser/.config/qutebrowser/config.py
+++ qutebrowser/.config/qutebrowser/config.py
@@ -7,12 +7,12 @@ config.bind('xg', 'tab-give') # break tab into own window
 config.bind('zd', 'download-open')
 config.bind('xb', 'config-cycle statusbar.hide')
 config.bind('xs', 'config-source') # re-source this config file
-config.bind('zm', 'spawn --detach mpv --fs --speed=1.33 --force-window yes {url}') # send current page to mpv to play video
-config.bind('zy', 'hint links spawn --detach mpv --fs --speed=1.33 --force-window yes {hint-url}') # send current page to mpv to play video
+config.bind('xm', 'spawn --detach mpv --fs --speed=1.33 --force-window yes {url}') # send current page to mpv to play video
+config.bind('xy', 'hint links spawn --detach mpv --fs --speed=1.33 --force-window yes {hint-url}') # send current page to mpv to play video
 
 # switch open bindings as open in new tab is the more useful binding
-config.bind('O', 'set-cmd-text -s :open ')
 config.bind('o', 'set-cmd-text -s :open -t ')
+config.bind('O', 'set-cmd-text -s :open ')
 
 # c.### are options set at launch
 c.content.cookies.accept    = 'all'
@@ -20,7 +20,7 @@ c.content.geolocation       = 'ask'
 c.content.webgl             = True
 c.downloads.remove_finished = 800
 c.auto_save.session         = True
-c.editor.command            = ["termite", "-e", "nvim '{}'"]
+c.editor.command            = ["st", "-e", "vim '{}'"]
 c.scrolling.smooth          = True
 c.content.autoplay          = False
 
@@ -39,27 +39,29 @@ c.tabs.select_on_remove     = 'prev' # previous tab is in the direction of paren
 
 # search engine shortneners
 c.url.searchengines = {
-    "DEFAULT": "https://duckduckgo.com/?q={}",
-    "g": "https://www.google.co.uk/search?&q={}",
-    "gi": "https://www.google.co.uk/search?q={}&tbm=isch",
-    "wiki": "https://en.wikipedia.org/w/index.php?search={}",
-    "steam": "http://store.steampowered.com/search/?term={}",
-    "aur": "https://aur.archlinux.org/packages/?O=0&K={}",
-    "arch": "https://wiki.archlinux.org/index.php?title=Special%3ASearch&search={}",
-    "syn": "https://www.thesaurus.com/browse/{}",
-    "ant": "https://www.thesaurus.com/browse/{}",
-    "imdb": "http://www.imdb.com/find?ref_=nv_sr_fn&s=all&q={}",
-    "dic": "http://www.dictionary.com/browse/{}",
-    "ety": "http://www.etymonline.com/index.php?allowed_in_frame=0&search={}",
-    "urban": "http://www.urbandictionary.com/define.php?term={}",
-    "yt": "https://www.youtube.com/results?search_query={}",
-    "ddgi": "https://duckduckgo.com/?q={}&iar=images",
-    "lutris": "https://lutris.net/games/?q={}",
-    "deal": "https://isthereanydeal.com/search/?q={}",
-    "gog": "https://www.gog.com/games?sort=popularity&search={}&page=1",
-    "proton": "https://www.protondb.com/search?q={}",
-    "qwant": "https://www.qwant.com/?q={}",
-    "itch": "https://itch.io/search?q={}"
+    "DEFAULT":  "https://duckduckgo.com/?q={}",
+    "d":        "https://duckduckgo.com/?q={}",
+    "ddg":      "https://duckduckgo.com/?q={}",
+    "g":        "https://www.google.co.uk/search?&q={}",
+    "gi":       "https://www.google.co.uk/search?q={}&tbm=isch",
+    "wiki":     "https://en.wikipedia.org/w/index.php?search={}",
+    "steam":    "http://store.steampowered.com/search/?term={}",
+    "aur":      "https://aur.archlinux.org/packages/?O=0&K={}",
+    "arch":     "https://wiki.archlinux.org/index.php?title=Special%3ASearch&search={}",
+    "syn":      "https://www.thesaurus.com/browse/{}",
+    "ant":      "https://www.thesaurus.com/browse/{}",
+    "imdb":     "http://www.imdb.com/find?ref_=nv_sr_fn&s=all&q={}",
+    "dic":      "http://www.dictionary.com/browse/{}",
+    "ety":      "http://www.etymonline.com/index.php?allowed_in_frame=0&search={}",
+    "urban":    "http://www.urbandictionary.com/define.php?term={}",
+    "yt":       "https://www.youtube.com/results?search_query={}",
+    "ddgi":     "https://duckduckgo.com/?q={}&iar=images",
+    "lutris":   "https://lutris.net/games/?q={}",
+    "deal":     "https://isthereanydeal.com/search/?q={}",
+    "gog":      "https://www.gog.com/games?sort=popularity&search={}&page=1",
+    "proton":   "https://www.protondb.com/search?q={}",
+    "qwant":    "https://www.qwant.com/?q={}",
+    "itch":     "https://itch.io/search?q={}"
 }
 
 # call my folder shortcuts scripts
